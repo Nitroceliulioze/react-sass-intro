bd97dc21749f8c5847b275fdb2c1bdde875626ed
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-10-03 17:22:09 +0300

    sass intro

---
+++ src/components/Mainpage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Styles/MainPage.css'
+
+const Mainpage = () => {
+  return (
+  <>
+    <div className='mainPage'>
+        <h1>Welcome to the website</h1>
+        <button>Click me</button>
+    </div>
+    
+    </>
+  )
+}
+
+export default Mainpage
